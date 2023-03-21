49805480fc42f314a3be2506c9d89ab092abd622
manumurillo0430 <manumurillo33@gmail.com>
2023-03-21 15:34:02 +0200

    Fronend updated

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:8080'
-// process.env.NODE_ENV !== 'development'
-// ?
-
-// : 'https://petsgram-adoption-server.onrender.com'
+const BASE_URL =
+  process.env.NODE_ENV !== 'development'
+    ? 'http://localhost:8080'
+    : 'https://petsgram-adoption-server.onrender.com'
 
 export const GetReq = async (path) => {
   const rest = await axios.get(BASE_URL + path, {
