a4d274e1604c78e4ac4c144fd37262adb010ae2d
manumurillo0430 <manumurillo33@gmail.com>
2023-03-21 15:38:18 +0200

    Backend to deploy

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://www.petsgram-adoption.com/'],
     credentials: true,
   }),
 )
