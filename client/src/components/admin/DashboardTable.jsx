07e78b4a7ae1a5e0c70d7b4e2759301471321cbd
manumurillo0430 <manumurillo33@gmail.com>
2023-04-08 19:08:23 +0300

    Frontend to deploy

---
+++ client/src/components/admin/DashboardTable.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import { Table, Thead, Tbody, Tr, Th, Td, Flex } from '@chakra-ui/react'
+import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons'
+import { useTable, useSortBy } from 'react-table'
+
+export default function DashboardTable({ columns, data }) {
+  const caseInsensitiveSort = (row1, row2) => {
+    let r1 = row1 ? row1.toLowerCase() : ''
+    let r2 = row2 ? row2.toLowerCase() : ''
+
+    if (r1 < r2) return -1
+    if (r1 > r2) return 1
+    return 0
+  }
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { sortBy: [{ id: 'name', desc: false }] },
+      sortTypes: {
+        alphanumeric: (row1, row2, name) => {
+          if (name === 'picture') {
+            return 0
+          } else {
+            return caseInsensitiveSort(row1.values[name], row2.values[name])
+          }
+        },
+      },
+      disableSortRemove: true,
+    },
+    useSortBy,
+  )
+
+  return (
+    <Table
+      size="md"
+      variant="simple"
+      colorScheme="blackAlpha"
+      fontSize="1rem"
+      mt={8}
+      {...getTableProps()}
+    >
+      <Thead>
+        {headerGroups.map((headerGroup) => (
+          <Tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <Th
+                {...column.getHeaderProps(
+                  column.getSortByToggleProps({
+                    title: `Sort by ${column.Header}`,
+                  }),
+                )}
+              >
+                <Flex align="center" wrap="nowrap" gap={2} w="6rem">
+                  {column.render('Header')}
+                  {column.isSorted ? (
+                    column.isSortedDesc ? (
+                      <TriangleDownIcon />
+                    ) : (
+                      <TriangleUpIcon />
+                    )
+                  ) : null}
+                </Flex>
+              </Th>
+            ))}
+          </Tr>
+        ))}
+      </Thead>
+      <Tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row)
+          return (
+            <Tr {...row.getRowProps()}>
+              {row.cells.map((cell) => (
+                <Td {...cell.getCellProps()}>{cell.render('Cell')}</Td>
+              ))}
+            </Tr>
+          )
+        })}
+      </Tbody>
+    </Table>
+  )
+}
---
+++ client/src/components/admin/TableDataUsers.jsx
@@ -0,0 +1,85 @@
+import { useMemo } from 'react'
+import DashboardTable from './DashboardTable'
+import { NavLink } from 'react-router-dom'
+import { Link, Avatar } from '@chakra-ui/react'
+import { LinkIcon } from '@chakra-ui/icons'
+import { userRoleColor } from '../../utils/globals'
+
+export default function TableDataUsers({ users }) {
+  const data = useMemo(
+    () =>
+      users.map((user) => {
+        return {
+          picture: user.picture,
+          firstname: user.firstname,
+          lastname: user.lastname,
+          email: user.email,
+          role: user.role,
+          account: user.is_private,
+          date: user.created_at,
+          id: user.user_id,
+        }
+      }),
+    [],
+  )
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'User',
+        accessor: 'picture',
+        Cell: (e) => <Avatar src={e.value} />,
+      },
+      {
+        Header: 'Name',
+        accessor: (row) => `${row.firstname} ${row.lastname}`,
+        Cell: (e) => (
+          <Link as={NavLink} to={`/profile/${e.row.original.id}`}>
+            <LinkIcon mr={2} />
+            {e.value}
+          </Link>
+        ),
+      },
+      {
+        Header: 'E-Mail',
+        accessor: 'email',
+        Cell: (e) => <Link href={`mailto:${e.value}`}>{e.value}</Link>,
+      },
+      {
+        Header: 'Role',
+        accessor: 'role',
+        Cell: (e) => {
+          if (e.value == true) {
+            return (
+              <span style={userRoleColor(e.value)} className="small-caps">
+                Admin
+              </span>
+            )
+          } else
+            return (
+              <span style={userRoleColor(e.value)} className="small-caps">
+                User
+              </span>
+            )
+        },
+      },
+      {
+        Header: 'Account',
+        accessor: 'account',
+        Cell: (e) => {
+          if (e.value === 1) {
+            return <span className="small-caps">Private</span>
+          } else return <span className="small-caps">Public</span>
+        },
+      },
+      {
+        Header: 'Created',
+        accessor: 'date',
+        Cell: (e) => <span>{new Date(e.value).toLocaleDateString()}</span>,
+      },
+    ],
+    [],
+  )
+
+  return <DashboardTable data={data} columns={columns} />
+}
