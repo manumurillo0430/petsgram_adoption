3d3b82ae2e3f0707aa45e44f071f91bed6037cfc
Manuel Murillo <105782380+manumurillo0430@users.noreply.github.com>
2023-04-20 18:15:52 +0300

    Delete server directory

--- server/Routes/authRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const { authenticateUser } = require('../Controllers/authController')
-const { auth } = require('../Middlewares/auth')
-const { getUsersLikesAuth } = require('../Middlewares/getUserLikesAuth')
-const router = express.Router()
-
-router.get('/', auth, getUsersLikesAuth, authenticateUser)
-
-module.exports = router
