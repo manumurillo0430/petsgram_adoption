ee3682c426b2047f75541774834bcc5394b8c1fd
Manuel Murillo <105782380+manumurillo0430@users.noreply.github.com>
2023-04-20 18:15:36 +0300

    Delete client directory

--- client/src/pages/AdminDashboard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import DashboardTemplate from '../components/admin/DashboardContainer.jsx '
-export default function AdminDashboard() {
-  return (
-    <>
-      <DashboardTemplate />
-    </>
-  )
-}
--- client/src/private_routes/PrivateRouteAdmin.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom'
-
-export default function PrivateRouteAdmin({ children }) {
-  const role = localStorage.getItem('userRole')
-
-  if (!role) return <Navigate to="/" replace />
-  else return children
-}
