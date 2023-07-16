81a4fef9e0507771170a423757c536b26d01738d
manumurillo0430 <manurillo33@gmail.com>
2023-07-16 21:09:36 +0300

    Changes to deploy

--- server/knex/knexfile.js
+++ server/knex/knexfile.js
@@ -4,12 +4,7 @@ module.exports = {
   client: "pg",
   connection: {
     connectionString:
-<<<<<<< HEAD
       "postgres://pestgramdatabase_s9kd_user:JOzPb2HmprdX7qJKN24UXRpcBC8LdRip@dpg-ciq2ujt9aq0dcpqbsiag-a.oregon-postgres.render.com/pestgramdatabase_s9kd",
-=======
-      'postgres://petsgram:Ywej6cyWCL8oKPd9pmm3dR6CYfk2hanJ@dpg-cimimi5gkuvgvh8pa710-a.oregon-postgres.render.com/pestgramdatabase',
-
->>>>>>> origin/main
     ssl: {
       rejectUnauthorized: false,
     },
